964caa31c0fa4d86b6dbda15c91b6cb3f4ff6984
sumeyra <ozugursumeyra@gmail.com>
2022-08-28 23:44:43 +0300

    data types

--- Tutorial/app/src/main/java/com/example/tutorial/tutorial.kt
+++ Tutorial/app/src/main/java/com/example/tutorial/tutorial.kt
@@ -1,14 +1,16 @@
 fun main(){
 
-    val interestingThings = arrayOf("kotlin","java","python")
-    interestingThings.size //3
-    interestingThings[0] //kotlin
-    interestingThings.get(0)// kotlin
+    val interestingThings = listOf("kotlin","java","python") // you dont add anythings
+    val interestingThings1 = mutableListOf("kotlin","java","python") // you can add somethings
+    interestingThings1.add("Go")
 
 
-    interestingThings.forEachIndexed { index, interestingThing ->
-        println("$interestingThing is at index $index")  // kotlin is at index 0 ...
-    }
+    val map= mapOf(1 to "a", 2 to "b",3 to "c")// you dont add anythings
+    map.forEach{(key, value) -> println("$key -> $value")}
+
+    val map1= mutableMapOf(1 to "a", 2 to "b",3 to "c")// you dont add anythings
+    map1.put(4,"d")// you can add somethings
+    map1.forEach{(key, value) -> println("$key -> $value")}
 
 }
 
