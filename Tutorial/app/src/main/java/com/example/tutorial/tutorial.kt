e5638a1d19c23f226cd300fcc43c9fc7a3d85b2c
sumeyra <ozugursumeyra@gmail.com>
2022-08-28 23:04:30 +0300

    data types array

--- Tutorial/app/src/main/java/com/example/tutorial/tutorial.kt
+++ Tutorial/app/src/main/java/com/example/tutorial/tutorial.kt
@@ -1,16 +1,19 @@
-
-fun sayHello(name:String){
-    println("Hello, ${name}")
-
-}
-
-fun saySomethin(greeting:String, name:String) = println("$greeting,$name")
-
-
-
 fun main(){
-    sayHello("Sumeyra")
-    saySomethin("Hey","Sumeyra")
+
+    val interestingThings = arrayOf("kotlin","java","python")
+    println(interestingThings.size) //3
+    println(interestingThings[0]) //kotlin
+    println(interestingThings.get(0)) // kotlin
+
+
+    for (interestingThings in interestingThings){
+        println(interestingThings) // kotlin, java,python
+    }
+
+    //Best way
+    interestingThings.forEach {
+        println(it) // kotlin, java, python
+    }
 
 }
 
