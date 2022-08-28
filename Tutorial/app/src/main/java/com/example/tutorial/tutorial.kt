2fb05fe122f5818eeabbfd3639965abd4b185175
sumeyra <ozugursumeyra@gmail.com>
2022-08-28 23:09:21 +0300

    data types array 2

--- Tutorial/app/src/main/java/com/example/tutorial/tutorial.kt
+++ Tutorial/app/src/main/java/com/example/tutorial/tutorial.kt
@@ -1,18 +1,13 @@
 fun main(){
 
     val interestingThings = arrayOf("kotlin","java","python")
-    println(interestingThings.size) //3
-    println(interestingThings[0]) //kotlin
-    println(interestingThings.get(0)) // kotlin
+    interestingThings.size //3
+    interestingThings[0] //kotlin
+    interestingThings.get(0)// kotlin
 
 
-    for (interestingThings in interestingThings){
-        println(interestingThings) // kotlin, java,python
-    }
-
-    //Best way
-    interestingThings.forEach {
-        println(it) // kotlin, java, python
+    interestingThings.forEachIndexed { index, interestingThing ->
+        println("$interestingThing is at index $index")  // kotlin is at index 0 ...
     }
 
 }
