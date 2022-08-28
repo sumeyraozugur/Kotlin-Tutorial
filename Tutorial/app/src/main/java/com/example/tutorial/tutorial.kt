0fdaae154e24ef2879471b259d480210ac5b6649
sumeyra <ozugursumeyra@gmail.com>
2022-08-28 23:59:43 +0300

    example

--- Tutorial/app/src/main/java/com/example/tutorial/tutorial.kt
+++ Tutorial/app/src/main/java/com/example/tutorial/tutorial.kt
@@ -1,16 +1,25 @@
+
+fun sayHello(greeting:String, itemsToGreet:List<String>){
+
+    itemsToGreet.forEach{ item ->
+        println("$greeting,$item")
+
+
+    }
+
+
+
+}
+
+
 fun main(){
 
     val interestingThings = listOf("kotlin","java","python") // you dont add anythings
-    val interestingThings1 = mutableListOf("kotlin","java","python") // you can add somethings
-    interestingThings1.add("Go")
+    sayHello("Hi",interestingThings)
 
 
-    val map= mapOf(1 to "a", 2 to "b",3 to "c")// you dont add anythings
-    map.forEach{(key, value) -> println("$key -> $value")}
 
-    val map1= mutableMapOf(1 to "a", 2 to "b",3 to "c")// you dont add anythings
-    map1.put(4,"d")// you can add somethings
-    map1.forEach{(key, value) -> println("$key -> $value")}
+
 
 }
 
