d8dd861579c955d1d9798026bbb123750c0cec38
sumeyra <ozugursumeyra@gmail.com>
2022-08-29 18:52:44 +0300

    companion object

---
+++ Tutorial/app/src/main/java/com/example/tutorial/Entity.kt
@@ -0,0 +1,25 @@
+package com.example.tutorial
+
+interface IdProvider{
+    fun getId():String
+}
+
+class Entity private constructor(val id:String) {
+
+    companion object Factory:IdProvider{
+
+        override fun getId(): String {
+            return  "123"
+        }
+        const val id ="id"
+
+        fun create() = Entity(getId())
+
+    }
+}
+
+fun main(){
+
+    val entity = Entity.create()
+
+}
